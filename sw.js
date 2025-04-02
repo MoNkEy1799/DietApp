417046e947b90d9198f49cc7ed1c8a4b76f0b70d
MoNkEy1799 <steam1799@pm.me>
2025-04-02 18:14:50 +0200

    small fixes

--- sw.js
+++ sw.js
@@ -1,28 +1,29 @@
-const CACHE_NAME = "dietcache-v1";
+const CACHE_NAME = "dietcache-v5";
 const CACHE_ASSETS = [
-  "/manifest.json",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/chart.js",
-  "/image.ico",
-  "/image.svg"
+    "/manifest.json",
+    "/index.html",
+    "/styles.css",
+    "/app.js",
+    "/chart.js",
+    "/image.ico",
+    "/image.svg"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_ASSETS))
-  );
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHE_ASSETS))
+    );
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((names) =>
-      Promise.all(names.map((name) => (name !== CACHE_NAME ? caches.delete(name) : null)))
-    )
-  );
+    event.waitUntil(
+        caches.keys().then((names) =>
+            Promise.all(names.map((name) => (name !== CACHE_NAME ? caches.delete(name) : null)))
+    ));
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request));
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
 });
