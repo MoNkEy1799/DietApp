7bcb431072fbdc86e7a18c718593e971e9df4ae6
MoNkEy1799 <MoNKEy1799@users.noreply.github.com>
2025-04-10 15:59:33 +0200

    added run install bat

---
+++ run_install_server.bat
@@ -0,0 +1,14 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+for /f "tokens=2 delims=:" %%A in ('ipconfig ^| findstr "IPv4 Address"') do (
+    set ip=%%A
+)
+
+for /f "tokens=* delims= " %%A in ("!ip!") do set ip=%%A
+
+echo Install app under: http://%ip%:8000
+python -m http.server 8000
+
+endlocal
